187b7105fc911b51c9e422f10409aa8e1a875555
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2019-02-06 15:08:57 -0600

    Update Menu btn

--- WorldTour/index.vr.js
+++ WorldTour/index.vr.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text, Pano, AppRegistry, asset } from 'react-vr';
+import { View, Text, Pano, AppRegistry, asset, StyleSheet } from 'react-vr';
 
 class WorldTour extends Component {
   render() {
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')}></Pano>
+        <View style={styles.menuButton}>
+          <Text style={styles.menuButtonText}>Open Menu</Text>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  menuButton: {
+    backgroundColor: '#fff',
+    borderRadius: 0.25,
+    width: 0.5,
+    height: 0.5,
+    alignItems: 'center',
+    borderWidth: 0.01,
+    transform: [
+      {translate: [-2, 0, -5]}
+    ]
+  },
+  menuButtonText: {
+    textAlign: 'center',
+    fontSize: 0.15,
+    color: '#000'
+  }
+})
 
 AppRegistry.registerComponent('WorldTour', () => WorldTour);
