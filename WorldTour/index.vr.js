20f0309715f23149b5d5359dfffdda912ca3fb21
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2019-02-06 14:55:28 -0600

    Update index.vr.js

--- WorldTour/index.vr.js
+++ WorldTour/index.vr.js
@@ -1,34 +1,14 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import React, { Component } from 'react';
+import { View, Text, Pano, AppRegistry, asset } from 'react-vr';
 
-export default class WorldTour extends React.Component {
+class WorldTour extends Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+        <Pano source={asset('starry-sky.jpg')}></Pano>
       </View>
-    );
+    )
   }
-};
+}
 
 AppRegistry.registerComponent('WorldTour', () => WorldTour);
